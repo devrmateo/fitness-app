d601ba7e276e18a2d6dab1f09cdee8deeb893833
Matthew Smith <mateoelcule@gmail.com>
2019-04-24 10:25:09 -0700

    feat: Add TextButton to AddEntry

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,6 +4,8 @@ import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import UdaciSlider from './Slider'
 import UdaciSteppers from './Steppers'
 import DateHeader from './DateHeader'
+import { Ionicons } from '@expo/vector-icons'
+import TextButton from './TextButton'
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -76,8 +78,33 @@ export default class AddEntry extends Component {
 
   }
 
+  reset = () => {
+    const key = timeToString()
+
+    //Update Redux
+
+    //Route to home
+
+    //Update 'DB'
+  }
+
   render() {
     const metaInfo = getMetricMetaInfo()
+
+    if (true) {
+      return (
+        <View>
+          <Ionicons
+            name='ios-happy'
+            size={100}
+          />
+          <Text>You already logged your information for today.</Text>
+          <TextButton onPress={this.reset}>
+            Reset
+          </TextButton>
+        </View>
+      )
+    }
     return (
       <View>
         <DateHeader date={(new Date()).toLocaleDateString()}/>
