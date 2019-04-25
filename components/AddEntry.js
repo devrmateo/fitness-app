8dfa5c4063a401cd93a8a414eec97470f3307251
Matthew Smith <mateoelcule@gmail.com>
2019-04-25 10:05:54 -0700

    feat: Add action creators for Redux

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -92,7 +92,7 @@ export default class AddEntry extends Component {
   render() {
     const metaInfo = getMetricMetaInfo()
 
-    if (true) {
+    if (this.props.alreadyLogged) {
       return (
         <View>
           <Ionicons
