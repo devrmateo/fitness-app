a76c910f33c840c3fbd861c10688cf3d9ce41df0
Matthew Smith <mateoelcule@gmail.com>
2019-04-29 11:11:43 -0700

    feat: Style date, sliders and reset button

--- components/Slider.js
+++ components/Slider.js
@@ -1,20 +1,46 @@
 import React from 'react'
-import { View, Text, Slider } from 'react-native'
+import { View, Text, Slider, StyleSheet } from 'react-native'
+import { gray } from '../utils/colors'
 
 export default function UdaciSlider ({ max, unit, step, value, onChange }) {
   return (
-    <View>
+    <View style={styles.row}>
       <Slider
+        style={styles.slider}
         step={step}
         value={value}
         maximumValue={max}
         minimumValue={0}
         onValueChange={onChange}
       />
-      <View>
-        <Text>{value}</Text>
-        <Text>{unit}</Text>
+      <View style={styles.metricCounter}>
+        <Text style={styles.metricNumber}>{value}</Text>
+        <Text style={{ color: gray }}>{unit}</Text>
       </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  slider: {
+    flex: 1,
+    marginRight: 10,
+  },
+  metricCounter: {
+    flexDirection: 'row',
+    width: 85,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  metricNumber: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+    marginRight: 5,
+  },
+})
