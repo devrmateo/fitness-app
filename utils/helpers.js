c88db55166b460260b46e3cb99a031d407aec92a
Matthew Smith <mateoelcule@gmail.com>
2019-05-07 13:28:10 -0700

    feat: Begin styling views in History component

--- utils/helpers.js
+++ utils/helpers.js
@@ -158,6 +158,6 @@ export function getMetricMetaInfo (metric) {
 
 export function getDailyReminderValue () {
   return {
-    today: '👋 Don\'t forget to log your data today!',
+    today: '👋 Don\'t forget to log your data for today!',
   }
 }
