38c8b6d63ae0e4fc8768d841aacb42ca75393460
Matthew Smith <mateoelcule@gmail.com>
2019-05-09 11:59:03 -0700

    feat: Add AppLoading component for initial load

--- components/History.js
+++ components/History.js
@@ -8,8 +8,13 @@ import UdaciFitnessCalendar from 'udacifitness-calendar'
 import { white } from '../utils/colors'
 import DateHeader from './DateHeader'
 import MetricCard from './MetricCard'
+import { AppLoading } from 'expo'
 
 class History extends Component {
+  state = {
+    ready: false,
+  }
+
   componentDidMount() {
     const { receiveEntries, addEntry } = this.props
     fetchCalendarResults()
@@ -21,6 +26,9 @@ class History extends Component {
         })
       }
     })
+    .then(() => this.setState({
+      ready: true,
+    }))
   }
 
   renderItem = ({ today, ...metrics }, formattedDate, key) => (
@@ -36,7 +44,6 @@ class History extends Component {
               metrics={metrics}
             />
           </TouchableOpacity>
-
       }
     </View>
   )
@@ -52,6 +59,12 @@ class History extends Component {
 
   render() {
     const { entries } = this.props
+    const { ready } = this.state
+
+    if (ready === false) {
+      return <AppLoading />
+    }
+
     return (
       <UdaciFitnessCalendar
         items={entries}
